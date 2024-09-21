34d84895b8ad67ffa9d901fded9a382d3a5e1f27
Priya Rathore <rathorepriya1705@gmail.com>
2024-09-21 20:45:11 +0530

    Change baseurl

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //defining base url
-const baseURL = "http://localhost:8000/";
+const baseURL = "https://payment-app-backend-eosin.vercel.app/";
 
 // Create Axios instance
 const instance = axios.create({
